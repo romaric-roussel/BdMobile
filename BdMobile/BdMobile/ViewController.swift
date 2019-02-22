d2606c1ce1e324720e21df54b27c54ac33b005b3
romaric roussel <romaric.roussel@outlook.fr>
2019-02-22 10:36:49 +0100

    list todo

--- BdMobile/BdMobile/ViewController.swift
+++ BdMobile/BdMobile/ViewController.swift
@@ -15,21 +15,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        setupConstraints()
+        //setupConstraints()
     }
     
     func  setupConstraints() {
         
         navigationBar.translatesAutoresizingMaskIntoConstraints = false
         navigationBar.heightAnchor.constraint(equalToConstant:44).isActive = true
-        navigationBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        navigationBar.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
-        navigationBar.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+        navigationBar.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        navigationBar.rightAnchor.constraint(equalTo:view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        navigationBar.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
 
 
         
     }
 
 
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier")!
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 }
 

9550486f33e5a7207284cb7c57b903406744401c
romaric roussel <romaric.roussel@outlook.fr>
2019-02-22 15:19:33 +0100

    CheckListItem

---
+++ BdMobile/BdMobile/CheckListItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  CheckListItemCell.swift
+//  BdMobile
+//
+//  Created by lpiem on 22/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class CheckListItemCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
